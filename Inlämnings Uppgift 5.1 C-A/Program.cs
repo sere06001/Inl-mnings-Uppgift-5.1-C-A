b40a5a857208c8ad3a3f03aaaa738e0701e4b5bb
sere06001 <sere06001@utb.vaxjo.se>
2023-11-22 16:00:06 +0100

    Add project files.

---
+++ Inlämnings Uppgift 5.1 C-A/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Uppgift5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Detta programm skriver ut några meningar.");
+            string[] meningar = { "Jag tycker om att programmera.", "Detta är andra meningen i array:en.", "Jag är klar med uppgiften." };
+            for (int i = 0; i < meningar.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine(meningar[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
